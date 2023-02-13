477a3b528d2f4557cff79f812dd0a7bd31be1b76
Pratik JH <56499739+PratikJH153@users.noreply.github.com>
2023-02-13 13:00:35 +0530

    Added required packages and modules

---
+++ src/models/test.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { ITest } from '../interfaces/ITest';
+
+const Test = new mongoose.Schema(
+  {    
+    name: {
+        type: String,
+        required: [true, "name is required"],
+        default: "name",
+
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<ITest & mongoose.Document>('ITest', Test);
