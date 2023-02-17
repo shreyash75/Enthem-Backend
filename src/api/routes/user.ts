036fec54bdcc0f63ee2bf444e63731c64a0c67f4
Pratik JH <56499739+PratikJH153@users.noreply.github.com>
2023-02-17 13:11:38 +0530

    Changed user routes

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -4,14 +4,65 @@ import config from "../../config";
 
 const route = Router();
 
+// email
+// Photo -> URL
+// Username
+// Gender
+// DOB
+// Skills
+// Interests
+// Geo Location 
+
 //! Solve Container.get(logger) issue
 export default (app: Router) => {
     app.use('/user', route);
 
     const db = driver(config.databaseURL, auth.basic(config.dbUser, config.dbPass),
-        {/* encrypted: 'ENCRYPTION_OFF' */ });
-
+        {/* encrypted: 'ENCRYPTION_OFF' */ },);
+    
+    const session = db.session({ database: "neo4j" });
+        
+    // Get User Data
     route.get('/', async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const query =
+            `
+            MATCH (n:User)
+            RETURN n;
+            `;
+            
+            const result = await session.run(query);
+            console.log("RESULT:");
+            result.records.forEach(i=>console.log(i.get("n").properties));
+            
+            return res.status(201).json({ "Server Working": true });
+        } catch (e) {
+            return next(e);
+        }
+    });  
+
+    // Create User Data
+    route.post('/', async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const query =
+                `
+                MATCH (n:User)
+                RETURN n;
+                `;
+    
+            const session = db.session({ database: "neo4j" });
+            const result = await session.run(query);
+            console.log("RESULT:");
+            result.records.forEach(i=>console.log(i.get("n").properties));
+    
+            return res.status(201).json({ "Server Working": true });
+        } catch (e) {
+            return next(e);
+        }
+    });
+
+    // Update User Data
+    route.put('/', async (req: Request, res: Response, next: NextFunction) => {
         try {
             const query =
                 `
@@ -28,5 +79,25 @@ export default (app: Router) => {
         } catch (e) {
             return next(e);
         }
-    });
+    });    
+
+    // Delete User Data
+    route.delete('/', async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const query =
+                `
+                MATCH (n:User)
+                RETURN n;
+                `;
+
+            const session = db.session({ database: "neo4j" });
+            const result = await session.run(query);
+            console.log("RESULT:");
+            result.records.forEach(i=>console.log(i.get("n").properties));
+
+            return res.status(201).json({ "Server Working": true });
+        } catch (e) {
+            return next(e);
+        }
+    });    
 };
